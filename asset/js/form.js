09431066e1c5ec2c52c14c5afbeacf7cbb392604
Jim Safley <jimsafley@gmail.com>
2021-01-25 13:24:57 -0500

    Add reset button

--- asset/js/form.js
+++ asset/js/form.js
@@ -1,19 +1,37 @@
 document.addEventListener('DOMContentLoaded', e => {
+
     const form = document.getElementById('collecting-together-form');
+    const resetButton = document.getElementById('collecting-together-reset');
     const projects = document.getElementById('collecting-together-projects');
     const cfSelect = document.getElementById('cf-select');
     const gfSelect = document.getElementById('gf-select');
     const mfSelect = document.getElementById('mf-select');
     const amSelect = document.getElementById('am-select');
 
+    // Prepare the initial state of the form
+    setSavedState();
+    filterProjects()
+
+    // Prevent form submission
     form.addEventListener('submit', e => e.preventDefault());
+
+    // Handle changes to the form
     form.addEventListener('change', e => {
         saveState();
         filterProjects();
     });
-    setSavedState();
-    filterProjects()
 
+    // Handle the reset button
+    resetButton.addEventListener('click', e => {
+        cfSelect.value = '';
+        gfSelect.value = '';
+        mfSelect.value = '';
+        amSelect.value = '';
+        saveState();
+        filterProjects();
+    });
+
+    // Filter projects
     async function filterProjects() {
         query = {
             cf: cfSelect.value,
@@ -30,6 +48,7 @@ document.addEventListener('DOMContentLoaded', e => {
         projects.innerHTML = text;
     };
 
+    // Save the current state of the form
     function saveState() {
         const state = {
             cf: cfSelect.value,
@@ -40,6 +59,7 @@ document.addEventListener('DOMContentLoaded', e => {
         localStorage.setItem('collecting_together_form', JSON.stringify(state));
     };
 
+    // Set the saved state to the form
     function setSavedState() {
         const state = JSON.parse(localStorage.getItem('collecting_together_form'));
         if (state) {
